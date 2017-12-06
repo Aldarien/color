e0f02e3e14a52559db71a2abbf61b2079c4477e3
Aldarien <aldarien85@gmail.com>
2017-12-06 11:45:34 -0300

    Added 2 functions to color

--- src/Color.php
+++ src/Color.php
@@ -48,7 +48,7 @@ class Color
 	/**
 	 * 
 	 * @param float $value Lightness value
-	 * @param bool $percent if number es from [0, 100] or [0, 1]
+	 * @param bool $percent if number is from [0, 100] or [0, 1]
 	 */
 	public function setLightness(float $value, bool $percent = true)
 	{
@@ -90,6 +90,48 @@ class Color
 		}
 		throw new \InvalidArgumentException('Type ' . $type . ' not implemented.');
 	}
+	
+	/**
+	 * Darken the color by % value
+	 * @param double $value positive value to darken in [0, 1] or [0, 100] range.
+	 * @param boolean $percent (optional) if in [0, 1] or [0, 100] range.
+	 */
+	public function darken(double $value, boolean $percent = false)
+	{
+		if ($percent) {
+			$value /= 100;
+		}
+		if ($value < 0 or $value > 1) {
+			throw new \OutOfRangeException($percent . ' is out of [0, 1] range.');
+		}
+		$sat = $this->saturation->getValue();
+		$sat *= (1 - $value);
+		if ($sat < 0) {
+			$sat = 0;
+		}
+		$this->saturation->setValue($sat);
+	}
+	/**
+	 * Lighten the color by % value
+	 * @param double $value positive value to lighten in [0, 1] or [0, 100] range.
+	 * @param boolean $percent (optional) if in [0, 1] or [0, 100] range.
+	 */
+	public function lighten(double $value, boolean $percent = false)
+	{
+		if ($percent) {
+			$value /= 100;
+		}
+		if ($value < 0 or $value > 1) {
+			throw new \OutOfRangeException($percent . ' is out of [0, 1] range.');
+		}
+		$sat = $this->saturation->getValue();
+		$sat *= (1 + $value);
+		if ($sat > 100) {
+			$sat = 100;
+		}
+		$this->saturation->setValue($sat);
+	}
+	
 	public function __call($name, $args)
 	{
 		if (method_exists($this, $name)) {
