dffbbe4571cd63fd2f65f21ed94a2fd12d7e4ba4
Aldarien <aldarien85@gmail.com>
2017-12-13 13:13:47 -0300

    Final tests

--- tests/DefinitionTest.php
+++ tests/DefinitionTest.php
@@ -4,14 +4,41 @@ use Lib\Color\ColorFactory;
 
 class DefinitionTest extends TestCase
 {
+	protected function assertHex($result, $name)
+	{
+		$color = ColorFactory::color($name);
+		$color = $color->transform('hex');
+		$this->assertEquals($result, $color->hex);
+	}
 	public function testBlue()
 	{
 		$name = 'blue';
-		$color = ColorFactory::color($name);
-		$color = $color->transform('rgb');
-		$this->assertEquals(0, $color->red);
-		$this->assertEquals(0, $color->green);
-		$this->assertEquals(255, $color->blue);
+		$hex = 0x0000FF;
+		$this->assertHex($hex, $name);
+	}
+	public function testRed()
+	{
+		$name = 'red';
+		$hex = 0xFF0000;
+		$this->assertHex($hex, $name);
+	}
+	public function testGreen()
+	{
+		$name = 'green';
+		$hex = 0x008000;
+		$this->assertHex($hex, $name);
+	}
+	public function testBlack()
+	{
+		$name = 'black';
+		$hex = 0x0;
+		$this->assertHex($hex, $name);
+	}
+	public function testWhite()
+	{
+		$name = 'white';
+		$hex = 0xFFFFFF;
+		$this->assertHex($hex, $name);
 	}
 }
 ?>
